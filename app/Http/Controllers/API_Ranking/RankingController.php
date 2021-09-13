e7b5b5eb45ef305774c6e9b3ee5d0a4da20a8d5e
FilipeVMartins <filipe_2021@hotmail.com>
2021-09-13 00:12:43 -0300

    moved calculateDistance(Vaga,Candidato) function to Candidatura Model, created new column on candidaturas table, added hook on saving event of Candidatura Model to make score field computed and stored when creating or updating

--- app/Http/Controllers/API_Ranking/RankingController.php
+++ app/Http/Controllers/API_Ranking/RankingController.php
@@ -4,132 +4,20 @@ namespace App\Http\Controllers\API_Ranking;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Candidatura;
 
 class RankingController extends Controller
 {
-
-    public function __construct()
-    {
-
-        $this->mapaLocalidades = [
-            'A' => ['B'=>5],
-            'B' => ['A'=>5, 'C'=>7, 'D'=>3],
-            'C' => ['B'=>7, 'E'=>4],
-            'D' => ['B'=>3, 'E'=>10, 'F'=>8],
-            'E' => ['C'=>4, 'D'=>10],
-            'F' => ['D'=>8],
-        ];
-
-    }
-
-
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request, $id)
+    public function index($id)
     {
-
-        $distancia = $this->calculaDistancia('B', 'A');
-        
-
-
-        $data = 'rtwesf';
-        
-        //return response()->json($distancia);
+        $data = Candidatura::where('id_vaga', $id)->orderBy('score')->get();
+        return response()->json($data);
     }
 
 
-
-
-    private function calculaDistancia($localizacaoVaga, $localizacaoCandidato)
-    {
-        //verificar se local atual == local empresa, se local atual tem outros caminhos para além do ultimo local
-        //$localAtual = $this->mapaLocalidades[$localizacaoCandidato];
-
-        //$locaisPercorridos = [];
-        //$locaisPercorridos[] = $localizacaoCandidato;
-
-        //$this->encontraCaminhos($localAtual, $localizacaoCandidato, $rotasEncontradas);
-        
-        
-        //inicio, localidade B com distancia 0
-        $rotasEncontradas = [[$localizacaoCandidato=>0]];
-        while (true){
-            $rotasEncontradasOld = $rotasEncontradas;
-
-            foreach ($rotasEncontradas as $index => $rota){
-                $this->encontraRotas($this->mapaLocalidades[array_key_last($rota)], array_key_last($rota), $rotasEncontradas);
-            }
-
-            if ($rotasEncontradasOld === $rotasEncontradas){
-                break;
-            }
-        }
-        print_r($rotasEncontradas);
-
-        //calcular todas as distâncias encontradas nas rotas até $localizacaoVaga
-        $distancias = [];
-        foreach($rotasEncontradas as $index => $rota){
-            //verificar se o destino existe na rota antes de calcular
-            if(isset($rota[$localizacaoVaga])){
-                $totalDistanciaRota = 0;
-
-                foreach($rota as $localidade => $distancia){
-                    $totalDistanciaRota += $distancia;
-
-                    //se chegou ao destino, break
-                    if ($localidade == $localizacaoVaga){
-                        break;
-                    }
-                }
-                $distancias[] = $totalDistanciaRota;
-            }
-        }
-        //pegar a menor distância encontrada
-        $menorDistancia = min($distancias);
-        print_r($menorDistancia);
-
-        return $menorDistancia;
-    }
-
-    private function encontraRotas($localAtual, $localAtualKey, &$rotasEncontradas){
-
-        $locaisPercorridos = [];
-        //gerar $locaisPercorridos
-        foreach($rotasEncontradas as $index => $rota){
-            if (array_key_last($rota) == $localAtualKey){
-                foreach ($rota as $localName => $distancia){
-                    $locaisPercorridos[] = $localName;
-                }
-            }
-        }
-        //print_r($locaisPercorridos);
-
-        //$localAtualKey = end($locaisPercorridos);
-        $newRotasEncontradas = [];
-        $rotasEncontradasIndexesToDelete = [];
-
-        foreach ($localAtual as $local => $distancia){
-            //prox local não pode já ter sido percorrido //precisa existir um caminho no prox local/// && count($this->mapaLocalidades[$local]) != 0(n usado)      //se houver apenas 1 caminho no prox local, ele n pode estar nos já percorridos /// && !(count($this->mapaLocalidades[$local]) == 1 && in_array( array_key_first($this->mapaLocalidades[$local]) , $locaisPercorridos))(n usado)
-            if (!in_array($local, $locaisPercorridos)){
-                //$rotasEncontradas[] = [$local => $distancia];
-
-                foreach ($rotasEncontradas as $index => $rota){
-                    if (array_key_last($rota) == $localAtualKey){
-                        $newRotasEncontradas[] = array_merge($rota, [$local => $distancia]);
-                        $rotasEncontradasIndexesToDelete[] = $index;
-                    }
-                }
-            }
-            $locaisPercorridos[] = $local;
-        }
-
-        foreach($rotasEncontradasIndexesToDelete as $index => $value) {
-            unset($rotasEncontradas["$value"]);
-        }
-        
-        $rotasEncontradas = array_merge($rotasEncontradas, $newRotasEncontradas);
-    } 
 }
--- app/Models/Candidatura.php
+++ app/Models/Candidatura.php
@@ -18,4 +18,168 @@ class Candidatura extends Model
         'id_vaga',
         'id_pessoa',
     ];
+    // protected $hidden = [
+    //     'user',
+    // ];
+
+    //campos calculados
+    // protected $appends = ['score'];
+
+    // public function getScoreAttribute()
+    // {
+    //     return $this->calculaScore();
+    // }
+
+    //relacionamentos
+    public function vaga()
+    {
+        return $this->belongsTo(Vaga::class, 'id_vaga');
+    }
+
+    public function pessoa()
+    {
+        return $this->belongsTo(Pessoa::class, 'id_pessoa');
+    }
+
+
+
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::saving (function ($candidatura) {
+            $candidatura->score = $candidatura->calculaScore();
+        });
+    }
+
+
+
+
+
+
+
+
+    //calculo conforme especificação
+    private function calculaScore(){
+
+        //calcula N
+        $NV = $this->vaga->nivel;
+        $NC = $this->pessoa->nivel;
+        $N = 100 - 25 * ($NV - $NC);
+
+        //encontra a menor distância entre o candidato e a vaga
+        $distancia = intval($this->calculaDistancia($this->vaga->localizacao, $this->pessoa->localizacao));
+
+        //utiliza a distancia para definir o valor de D, seguindo a tabela da especificação
+        // 0 até 5	                100
+        // maiores que 5 até 10	    75
+        // maiores que 10 até 15	50
+        // maiores que 15 até 20	25
+        // maiores que 20	        0
+        if ($distancia <= 5) {
+            $D = 100;
+        } elseif ($distancia <= 10) {
+            $D = 75;
+        } elseif ($distancia <= 15) {
+            $D = 50;
+        } elseif ($distancia <= 20) {
+            $D = 25;
+        } elseif ($distancia > 20) {
+            $D = 0;
+        }
+
+        $score = ($N+$D)/2;
+        return $score;
+    }
+
+    protected $mapaLocalidades = [
+        'A' => ['B'=>5],
+        'B' => ['A'=>5, 'C'=>7, 'D'=>3],
+        'C' => ['B'=>7, 'E'=>4],
+        'D' => ['B'=>3, 'E'=>10, 'F'=>8],
+        'E' => ['C'=>4, 'D'=>10],
+        'F' => ['D'=>8],
+    ];
+
+    private function calculaDistancia($localizacaoVaga, $localizacaoCandidato)
+    {
+        //inicio, localidade B com distancia 0
+        $rotasEncontradas = [[$localizacaoCandidato=>0]];
+        while (true){
+            $rotasEncontradasOld = $rotasEncontradas;
+
+            foreach ($rotasEncontradas as $index => $rota){
+                $this->encontraRotas($this->mapaLocalidades[array_key_last($rota)], array_key_last($rota), $rotasEncontradas);
+            }
+
+            if ($rotasEncontradasOld === $rotasEncontradas){
+                break;
+            }
+        }
+        //print_r($rotasEncontradas);
+
+        //calcular todas as distâncias encontradas nas rotas até $localizacaoVaga
+        $distancias = [];
+        foreach($rotasEncontradas as $index => $rota){
+            //verificar se o destino existe na rota antes de calcular
+            if(isset($rota[$localizacaoVaga])){
+                $totalDistanciaRota = 0;
+
+                foreach($rota as $localidade => $distancia){
+                    $totalDistanciaRota += $distancia;
+
+                    //se chegou ao destino, break
+                    if ($localidade == $localizacaoVaga){
+                        break;
+                    }
+                }
+                $distancias[] = $totalDistanciaRota;
+            }
+        }
+        //pegar a menor distância encontrada
+        $menorDistancia = min($distancias);
+        //print_r($menorDistancia);
+
+        return $menorDistancia;
+    }
+
+    private function encontraRotas($localAtual, $localAtualKey, &$rotasEncontradas){
+
+        $locaisPercorridos = [];
+        //gerar $locaisPercorridos
+        foreach($rotasEncontradas as $index => $rota){
+            if (array_key_last($rota) == $localAtualKey){
+                foreach ($rota as $localName => $distancia){
+                    $locaisPercorridos[] = $localName;
+                }
+            }
+        }
+        //print_r($locaisPercorridos);
+
+        $newRotasEncontradas = [];
+        $rotasEncontradasIndexesToDelete = [];
+        foreach ($localAtual as $local => $distancia){
+            //prox local não pode já ter sido percorrido
+            if (!in_array($local, $locaisPercorridos)){
+                //$rotasEncontradas[] = [$local => $distancia];
+
+                foreach ($rotasEncontradas as $index => $rota){
+                    if (array_key_last($rota) == $localAtualKey){
+                        $newRotasEncontradas[] = array_merge($rota, [$local => $distancia]);
+                        $rotasEncontradasIndexesToDelete[] = $index;
+                    }
+                }
+            }
+            $locaisPercorridos[] = $local;
+        }
+
+        foreach($rotasEncontradasIndexesToDelete as $index => $value) {
+            unset($rotasEncontradas["$value"]);
+        }
+        $rotasEncontradas = array_merge($rotasEncontradas, $newRotasEncontradas);
+    }
 }
